b440dd2223945e2ec4ab46879b3fb7c8dc3b1c36
Daniel de Paula Porto <danielpporto@gmail.com>
2023-12-14 11:48:34 -0300

    Fix: Correção nos links da navbar

--- src/.vuepress/components/InstagramLink.ts
+++ src/.vuepress/components/InstagramLink.ts
@@ -4,15 +4,15 @@ import { h } from "vue";
 const InstagramLink: FunctionalComponent = () =>
   h(
     "div",
-    { class: "nav-item vp-repo" },
+    { class: "nav-item vp-repo instagram-link" },
     h("a", {
       class: "vp-repo-link",
-      href: "https://t.me/vuepressthemehope",
+      href: "https://www.instagram.com/romeunb?utm_source=qr",
       target: "_blank",
       rel: "noopener noreferrer",
       "aria-label": "instagram",
       innerHTML:
-        '<div class="instagram-link"><a href="https://www.instagram.com/romeunb?utm_source=qr" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a></div>',
+        '<i class="fab fa-instagram"></i>',
     }),
   );
 
